95151e4a76d21757bf458b6ef4a5a8ebfe94a105
Zachary E. Dahan <hello@zachary.pm>
2015-12-01 18:04:38 +0100

    Initial Commit, featuring Gulp. 🎉

---
+++ Gulpfile.js
@@ -0,0 +1,50 @@
+var options = {
+  "sassFiles"  : "./scss/**/*.scss",
+  "proxyURL" : "localhost/TechMe",
+  "cssPath"  : "./css/",
+  "jsFiles"  : "./js/**/*.js",
+  "jsMinPath": ".js-min/"
+};
+
+/**
+ * Gulp File for SASS
+ * v1.0.0
+ * Made by Zachary E. Dahan
+ *
+ * */
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+  browserSync.init({
+    proxy: options.proxyURL
+  });
+
+  gulp.watch(options.sassFiles, ['compile-sass']);
+  gulp.watch("./**/*.html").on('change', browserSync.reload);
+  gulp.watch("./**/*.js").on('change', browserSync.reload);
+  gulp.watch("./**/*.php").on('change', browserSync.reload);
+});
+
+gulp.task('compile-sass', function() {
+  gulp.src(options.sassFiles)
+    .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(options.cssPath))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js-min', function() {
+  return gulp.src(options.jsFiles)
+    .pipe(uglify())
+    .pipe(gulp.dest(''));
+});
