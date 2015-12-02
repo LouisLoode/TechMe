3e3d5bf96af74c7f202a93ecd2575c3c356d7f16
LouisLoode <contact@ldbrn.com>
2015-12-02 08:57:51 +0100

    Mini modifs app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-	 
+	var timerId =
+	  countdown(
+	    new Date(),
+	    function(ts) {
+	      document.getElementById('pageTimer').innerHTML = ts.toHTML("strong");
+	    },
+	    countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+
+	// later on this timer may be stopped
+	window.clearInterval(timerId);
+	
 	$('#countdown').countdown('2015/12/03 16:30:00', function(event) {
 		$(this).html(event.strftime('<span class="clock">%H</span>:<span class="clock">%M</span>:<span class="clock">%S</span>'));
 	});
