b2b7b662005356ba95f91429f9903cbd06861cb4
Valentin Crochemore <valentin.crochemore@gmail.com>
2015-12-02 14:11:01 +0100

    Debug de compte à rebours

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,10 @@
-$(document).ready(function(){$("#countdown").countdown("2015/12/03 16:30:00",function(n){$(this).html(n.strftime('<span class="clock">%H</span>:<span class="clock">%M</span>:<span class="clock">%S</span>'))})});
+$(document).ready(function(){
+	
+	$("#countdown").countdown("2015/12/03 15:00:00",function(n){
+		var totalHours = n.offset.totalDays * 24 + n.offset.hours;
+		$(this).html(n.strftime('<span class="clock">'+totalHours+'</span>:<span class="clock">%M</span>:<span class="clock">%S</span>'))
+		
+	});
+		
+		
+}); 
